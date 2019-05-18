d3fd30ee581718ec7af0a9e73c844973f86c6dbd
pdg2491@naver.com <vusehrjf!1>
2019-05-18 17:01:18 +0900

    [day1]study_day1 폴더 추가 & 파일 이동

--- NodeExample1/study_day1/ch02_test5/ch02_test5.js
+++ NodeExample1/study_day1/ch02_test5/ch02_test5.js
@@ -1,7 +1,7 @@
 // calc.js
-var calc = require('./calc');
+var calc = require( './calc');
 console.log('모듈로 분리한 후 - calc.add 함수 호출 결과 : %d', calc.add(10,10));
 
 // calc2.js
-var calc2 = require('./calc2');
+var calc2 = require( './calc2');
 console.log('모듈로 분리한 후 - calcs.add 함수 호출 결과 : %d', calc2.add(11, 11));
